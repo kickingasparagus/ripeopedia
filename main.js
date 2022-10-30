5c7e1908d08af50546d74579ff42f67da526dc91
Daniel <daniel@Daniels-MacBook-Pro.local>
2022-10-30 16:29:09 -0400

    Remove unnecessary JavaScript

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 "use strict";
 
-//import Swal from "sweetalert2/dist/sweetalert2.js"
-//
-//// CommonJS
-//const swal = require("sweetalert2")
-
 function outOfStockMessage() {
-    alert("We're very sorry. This item is out of stock :(")
-//    swal.fire({
-//      title: 'Error!',
-//      text: 'Do you want to continue',
-//      icon: 'error',
-//      confirmButtonText: 'Cool'
-//    })
+    alert("We're very sorry. This item is out of stock :(");
 }
